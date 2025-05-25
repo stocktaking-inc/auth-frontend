bb91ee470c0c83371121248fea65dc1b0c2a2171
Alexey Kuleshov <149209268+AlexxxeyKuleshov@users.noreply.github.com>
2025-05-26 02:11:14 +0300

    feat: replacing toast with sonner

---
+++ src/components/ui/lib/sonner.tsx
@@ -0,0 +1,8 @@
+import { Toaster as SonnerToaster, toast as sonnerToast } from 'sonner'
+import { cn } from '@/lib/utils.ts'
+
+const Sonner = () => {
+  return <SonnerToaster richColors position="bottom-right" className={cn('toaster group', 'max-w-md')} toastOptions={{ className: 'group toast' }} />
+}
+
+export { Sonner, sonnerToast }
