0534751cbde837d25c005282513ef376cd61655d
JlwxdQueen <jlwxd@mail.ru>
2025-05-25 17:39:04 +0300

    feat(nginx): reverse proxy for docker-based services

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,20 @@ import react from '@vitejs/plugin-react'
 import path from 'path'
 import { defineConfig } from 'vite'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/auth/",
+  server: {
+    host: "0.0.0.0",
+    port: 5173,
+    proxy: {
+      "/api/auth": {
+        target: "http://stocktaking-auth:5100",
+        changeOrigin: true,
+        secure: false
+      }
+    }
+  },
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src')
